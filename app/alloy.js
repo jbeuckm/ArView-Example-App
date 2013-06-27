e44b839e02a731b2326ed26c379dc9b83c825f65
Joe Beuckman <joseph.beuckman@usbank.com>
2013-06-27 10:30:52 -0500

    cleanup up alloy.js

--- app/alloy.js
+++ app/alloy.js
@@ -1,9 +1,7 @@
-var Alloy = require("/alloy");//, _ = Alloy._, Backbone = Alloy.Backbone;
+var Alloy = require("/alloy");
 
 Alloy.Globals.googleNearbyPlacesUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
 Alloy.Globals.googlePlaceDetailUrl = "https://maps.googleapis.com/maps/api/place/details/json"
 
 
-Alloy.Globals.googleApiKey = "AIzaSyDC3RWmkSYPEIyCPOI_DJGdLzPn7lrWh-E";
-
-//Alloy.Collections.GooglePlaces = Alloy.createCollection('GooglePlace');
+Alloy.Globals.googleApiKey = "YOUR_GOOGLE_API_KEY";
